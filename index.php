ae76661016593f691cb99a5c6b9c3a147a3cf29e
Hugo Rafael Lima <hugor.souzalima@gmail.com>
2018-11-16 17:23:46 -0300

    quiz without JS logic

---
+++ index.php
@@ -0,0 +1,351 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="robots" content="index, follow">
+	<title>Íbis - 80 anos</title>
+	<meta property="og:locale" content="pt_BR" />
+	<meta property="og:type" content="website" />
+	<meta property="og:title" content="Íbis - 80 anos" />
+	<meta property="og:description" content="" />
+	<meta property="og:url" content="https://www.op9.com.br/jogos/ibis80anos/" />
+	<meta property="og:site_name" content="Íbis - 80 anos" />
+	<meta property="og:image" content="https://www.op9.com.br/jogos/ibis80anos/images/ibis80anos_FB.png" />
+	<meta property="og:image:type" content="image/png" />
+	<meta property="og:image:width" content="1200" />
+	<meta property="og:image:height" content="630" />
+	<meta property="fb:app_id" content="282328752526719" />
+	<meta name="twitter:card" content="summary_large_image" />
+	<meta name="twitter:description" content="" />
+	<meta name="twitter:title" content="Íbis - 80 anos" />
+	<meta name="twitter:image" content="https://www.op9.com.br/jogos/ibis80anos/images/ibis80anos_FB.png" />
+	<link rel="stylesheet" type="text/css" href="./styles/style.css">
+
+	<?php /* ?>
+	<link rel="icon" href="https://media.op9.com.br/uploads/2018/03/favicon.png" sizes="32x32" />
+	<link rel="icon" href="https://media.op9.com.br/uploads/2018/03/favicon.png" sizes="192x192" />
+	<link rel="apple-touch-icon-precomposed" href="https://media.op9.com.br/uploads/2018/03/favicon.png" />
+	<meta name="msapplication-TileImage" content="https://media.op9.com.br/uploads/2018/03/favicon.png" />
+	<?php */ ?>
+	<script>
+		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+		  ga('create', 'UA-118143123-1', 'auto');
+		  ga('send', 'pageview');
+	</script>
+</head>
+<body>
+	<div id="content">
+		<div class="row">
+			<div class="four columns">
+				<section id="perguntas" style="display: none;">
+					<img class="logo" src="./images/logo-ibis.svg" alt="Logo Íbis">
+					<div class="wrap-perguntas">
+						<ul class="pergunta perg01 show" id="perg01">
+							<h1><span>1</span> Qual foi o time responsável por aplicar a maior goleada em uma só partida?</h1>
+							<li><button class="alt-a" onclick="rocking(this.id)" id="1"><i>&nbsp;</i><span>A</span> Sport</button></li>
+							<li><button class="alt-b" onclick="clay(this.id)" id="2"><i>&nbsp;</i><span>B</span> Náutico</button></li>
+							<li><button class="alt-c" onclick="couch(this.id)" id="3"><i>&nbsp;</i><span>C</span> Santa Cruz</button></li>
+							<li><button class="alt-d" onclick="couch(this.id)" id="4"><i>&nbsp;</i><span>D</span> Central</button></li>
+						</ul>
+						<ul class="pergunta perg02" id="perg02">
+							<h1><span>2</span> Em quais anos o Pássaro Preto foi ganhou a taça do Torneio Início?</h1>
+							<li><button class="alt-a" onclick="rocking(this.id)" id="5"><i>&nbsp;</i><span>A</span> 1948 e 1949</button></li>
+							<li><button class="alt-b" onclick="clay(this.id)" id="6"><i>&nbsp;</i><span>B</span> 1949 e 1950</button></li>
+							<li><button class="alt-c" onclick="couch(this.id)" id="7"><i>&nbsp;</i><span>C</span> 1947 e 1956</button></li>
+							<li><button class="alt-d" onclick="couch(this.id)" id="8"><i>&nbsp;</i><span>D</span> 1948 e 1950</button></li>
+						</ul>
+						<ul class="pergunta perg03" id="perg03">
+							<h1><span>3</span> Qual time foi a vítima do único gol marcado por Mauro Shampoo com a camisa do Íbis?</h1>
+							<li><button class="alt-a" onclick="rocking(this.id)" id="9"><i>&nbsp;</i><span>A</span> Ferroviário</button></li>
+							<li><button class="alt-b" onclick="clay(this.id)" id="10"><i>&nbsp;</i><span>B</span> Serrano</button></li>
+							<li><button class="alt-c" onclick="couch(this.id)" id="11"><i>&nbsp;</i><span>C</span> Central</button></li>
+							<li><button class="alt-d" onclick="couch(this.id)" id="12"><i>&nbsp;</i><span>D</span> Flamengo-PE</button></li>
+						</ul>
+						<ul class="pergunta perg04" id="perg04">
+							<h1><span>4</span> Quais desses clubes levou a maior goleada do Pior Time do Mundo?</h1>
+							<li><button class="alt-a" onclick="rocking(this.id)" id="13"><i>&nbsp;</i><span>A</span> Porto</button></li>
+							<li><button class="alt-b" onclick="clay(this.id)" id="14"><i>&nbsp;</i><span>B</span> Timbaúba</button></li>
+							<li><button class="alt-c" onclick="couch(this.id)" id="15"><i>&nbsp;</i><span>C</span> Ferroviário</button></li>
+							<li><button class="alt-d" onclick="couch(this.id)" id="16"><i>&nbsp;</i><span>D</span> Cabense</button></li>
+						</ul>
+						<ul class="pergunta perg05" id="perg05">
+							<h1><span>5</span> Qual foi a última vez que o Pássaro Preto disputou a Série A1?</h1>
+							<li><button class="alt-a" onclick="rocking(this.id)" id="17"><i>&nbsp;</i><span>A</span> 1999</button></li>
+							<li><button class="alt-b" onclick="clay(this.id)" id="18"><i>&nbsp;</i><span>B</span> 2000</button></li>
+							<li><button class="alt-c" onclick="couch(this.id)" id="19"><i>&nbsp;</i><span>C</span> 2001</button></li>
+							<li><button class="alt-d" onclick="couch(this.id)" id="20"><i>&nbsp;</i><span>D</span> 2002</button></li>
+						</ul>
+						<ul class="pergunta perg06" id="perg06">
+							<h1><span>6</span> Em que ano o clube disputou o Pernambucano pela primeira vez?</h1>
+							<li><button class="alt-a" onclick="rocking(this.id)" id="21"><i>&nbsp;</i><span>A</span> 1944</button></li>
+							<li><button class="alt-b" onclick="clay(this.id)" id="22"><i>&nbsp;</i><span>B</span> 1945</button></li>
+							<li><button class="alt-c" onclick="couch(this.id)" id="23"><i>&nbsp;</i><span>C</span> 1946</button></li>
+							<li><button class="alt-d" onclick="couch(this.id)" id="24"><i>&nbsp;</i><span>D</span> 1947</button></li>
+						</ul>
+					</div>
+				</section>
+				<section id="resultados" style="display: none;">
+					<img class="logo" src="./images/logo-ibis.svg" alt="Logo Íbis">
+					<div class="wrap-resultados">
+						<div class="res01" id="balanço">
+							<img src="./images/shampoo.svg" alt="Mauro Shampoo">
+							<h2>Barba, cabelo e bigode!</h2>
+							<p>Pense num fã do Passaro Pretro. Tu é o artilheiro que faltou ao Íbis!!!</p>
+						</div>
+						<div class="res02" id="barro">
+							<img src="./images/camisa.svg" alt="O mito, Nininho">
+							<h2>É bem dizer o mito Nininho!</h2>
+							<p>Não foi tão ruim, mas passou na peneira. Dá pra jogar na linha do Íbis!!</p>
+						</div>
+						<div class="res03" id="sofá">
+							<img src="./images/ibisdoibis.svg" alt="Íbis do Íbis">
+							<h2>O Íbis do Íbis!!!</h2>
+							<p>Cabra coerente com o quiz, nenhum golzinho? Danôsse Menino, sei não vú...</p>
+						</div>
+						<div class="wrap-respostas">
+							<a href="#" class="respostas">Respostas</a>
+							<img class="seta" src="./images/seta.svg" alt="">
+						</div>
+					</div>
+				</section>
+				<section id="respostas">
+					<ul>
+						<img class="logo" src="./images/logo-ibis80-respostas.svg" alt="Logo Íbis">
+						<li>
+							<h4>Resposta - 01</h4>
+							<p>Letra C. O Santa Cruz é o responsável pela maior goleada sofrida pelo Íbis. Não foi apenas uma, mas sim em duas oportunidades. Os tricolores venceram o Pássaro Preto por 13x0 em 1978 e em 1984.</p>
+						</li>
+
+						<li>
+							<h4>Resposta - 02</h4>
+							<p>Letra D. O Íbis sagrou-se campeão do Torneio Início nos anos de 1948 e 1950. O Pássaro Preto venceu o Náutico e Santa Cruz na disputa de pênaltis.</p>
+						</li>
+
+						<li>
+							<h4>Resposta - 03</h4>
+							<p>Letra A. Mauro Shampoo só conseguiu marcar um gol durante a sua trajetória como profissional, contra o Ferroviário. Mesmo com o gol marcado pelo craque, o Íbis saiu derrotado por 8x1.</p>
+						</li>
+
+						<li>
+							<h4>Resposta - 04</h4>
+							<p>Letra B. No ano de 2015, o Íbis e Timbaúba entraram em campo pela Série A2 do Pernambucano. O Pássaro Preto não tomou conhecimento do adversário e aplicou uma goleada por 8x2. O destaque ficou para o meia Moicano, que balançou as redes quatro vezes.</p>
+						</li>
+
+						<li>
+							<h4>Resposta - 05</h4>
+							<p>Letra B. O clube está há 18 anos longe da Série A1 do Campeonato Pernambucano. o Clube caiu de divisão no ano 2000 e desde então disputa a série A2.</p>
+						</li>
+						 
+						<li>
+							<h4>Resposta - 06</h4>
+							<p>Letra C. A primeira participação do Íbis no Campeonato Pernambucano foi em 1946. Na oportunidade, o troféu ficou nas Repúblicas Independentes do Arruda, com o Santa Cruz como campeão.</p>
+						</li>
+						<div class="wrap-voltar">
+							<a href="#" class="voltar">Voltar ao quiz</a>
+							<img class="seta" src="./images/seta.svg" alt="">
+						</div>
+					</ul>
+				</section>
+			</div>
+		</div>
+	</div>
+	<script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+	<script type="text/javascript">
+		/*
+		var a = 0;
+		var b = 0;
+		var c = 0;
+		var d = 0;
+		var i = 0;
+		var resp;
+		var quest = ["perg01", "perg02", "perg03", "perg04", "perg05", "perg06"];
+		var bread = ["b1", "b2", "b3", "b4", "b5", "b6"];
+
+		function rocking(id) {
+			a++;
+			console.log(a);
+
+			next();
+		}
+
+		function clay(id) {
+			b++;
+			console.log(b);
+
+			next();
+		}
+
+		function couch(id) {
+			c++;
+			console.log(c);
+
+			next();
+		}
+
+		function couch(id) {
+			d++;
+			console.log(d);
+
+			next();
+		}
+
+		function next() {
+
+			if (i >= 3) {
+
+				result();
+
+			} else {
+
+				document.getElementById(quest[i]).classList.remove("show");
+
+				i++;
+
+				document.getElementById(quest[i]).classList.add("show");
+				console.log(i);
+				document.getElementById(bread[i]).classList.add("selected");
+				return i;
+			
+			}
+		}
+
+		function change(id) {
+			
+			
+			if (bread[i] > id) { //going backwards
+
+				document.getElementById(bread[i]).classList.remove("selected");
+				document.getElementById(quest[i]).classList.remove("show");
+
+				--i;
+
+				check();
+				document.getElementById(quest[i]).classList.add("show");
+				console.log(i + ' id menor');
+				console.log(bread[i] + ' id menor');
+
+				return i;
+
+			}*/ /*else if (bread[i] < id) { //going forward
+
+				if (a || b || c > 0) {
+					document.getElementById(quest[i]).classList.remove("show");
+					document.getElementById(bread[i]).classList.add("selected");
+
+					++i;
+					
+					document.getElementById(quest[i]).classList.add("show");
+					document.getElementById(bread[i]).classList.remove("selected");
+					console.log(i + ' id maior');
+					console.log(bread[i] + ' id maior');
+
+					return i;
+
+				} else if (a || b || c <= 0) {
+					console.log('fool');
+					return i;
+				}
+
+			}*/ /*else {
+				console.log('hey im the same!');
+			}
+		}
+
+		function check() { //decrement on change
+			if (a > 1) {
+				a--;
+			} else if (b > 1) {
+				b--;
+			} else if (c > 1) {
+				c--;
+			} else if (d > 1) {
+				d--;
+			}
+		}
+
+		function result() {
+
+			document.getElementById("perguntas").style.display = "none";
+			document.getElementById("perguntas").style.visibility = "hidden";
+			document.getElementById("perguntas").style.opacity = "0";
+
+			if (a >= 2) {
+				console.log('Cadeira de balanço');
+				ShowResultsWrap();
+				ShowResultsBal();
+				document.getElementById("balanço").style.visibility = "visible";
+			} else if (b >= 2){
+				console.log('Filtro de Barro');
+				ShowResultsWrap();
+				ShowResultsBar();
+				document.getElementById("barro").style.visibility = "visible";
+			} else if (c >= 2) {
+				console.log('Sofá com manta');
+				ShowResultsWrap();
+				ShowResultsSof();
+				document.getElementById("sofá").style.visibility = "visible";
+			} else if (a < 2){
+				console.log('Cadeira de balanço');
+				ShowResultsWrap();
+				ShowResultsBal();
+				document.getElementById("balanço").style.visibility = "visible";
+			}
+		}
+
+		function ShowResultsWrap() {
+			document.getElementById("resultados").style.display = "block";
+			document.getElementById("resultados").style.visibility = "visible";
+			document.getElementById("resultados").style.opacity = "1";
+		}
+		function ShowResultsBal() {
+			document.getElementById("balanço").style.display = "block";
+			document.getElementById("balanço").style.visibility = "visible";
+			document.getElementById("balanço").style.opacity = "1";
+		}	
+		function ShowResultsBar() {
+			document.getElementById("barro").style.display = "block";
+			document.getElementById("barro").style.visibility = "visible";
+			document.getElementById("barro").style.opacity = "1";
+		}	
+		function ShowResultsSof() {
+			document.getElementById("sofá").style.display = "block";
+			document.getElementById("sofá").style.visibility = "visible";
+			document.getElementById("sofá").style.opacity = "1";
+		}
+
+		$(document).ready(function($) {
+			
+			var alturaTela = $(window).height();
+			var alturaTotal = alturaTela - 172;
+
+			$("#content").css({"height": alturaTotal+"px"});
+
+			// ADD CLASS ATIVO 
+			$("#perguntas li button").click(function(){
+				if ($(this).parent("li").siblings().hasClass("ativo")) {
+					$(this).parent("li").siblings().removeClass("ativo");
+				}
+				$(this).parent("li").addClass("ativo");
+			});
+
+		});
+
+		$(window).resize(function() {
+			var alturaTela = $(window).height();
+			var alturaTotal = alturaTela - 172;
+			
+			$("#content").css({"height": alturaTotal+"px"});
+
+		});*/
+	</script>
+		<script type="text/javascript" src="//platform-api.sharethis.com/js/sharethis.js#property=5b58800cd6d379001104abef&product=custom-share-buttons"></script>
+	<?php /* ?>
+	<script type='text/javascript' src='//platform-api.sharethis.com/js/sharethis.js#property=5b58800cd6d379001104abef&product=inline-share-buttons' async='async'></script>
+	<?php */ ?>
+</body>
+</html>
